97491729ede878067270a124f622f82021e6f4b7
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-04 14:53:03 -0700

    gets the create test and list test to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,60 @@
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
+const request = require('supertest');
+const app = require('../lib/app');
+const Bird = require('../lib/models/Bird');
 
 describe('app routes', () => {
-    const mongo - new MongoMemoryServer();
-    beforeAll(() => {
-        return mongo.getUri()
-        .then(uri => mongoose.connect(uri, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        }));
+  const mongo = new MongoMemoryServer();
+  beforeAll(() => {
+    return mongo.getUri()
+      .then(uri => mongoose.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      }));
+  });
+
+  beforeEach(() =>{
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('creates a new bird', () => {
+    return request(app)
+      .post('/birds')
+      .send({ bird: 'barn owl', season: 'summer' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          bird: 'barn owl',
+          season: 'summer',
+          spotted: 1,
+          __v: 0
+        });
+      });
+  });
+
+  it('gets a list of birds', async() => {
+    await Bird.create({
+      bird: 'sparrow',
+      season: 'fall'
     });
 
-    beforeEach(() =>{
-        return mongoose.connection.dropDatabase();
-    });
-
-    afterAll(() => {
-        return mongoose.connection.close();
-    });
+    return request(app)
+      .get('/birds')
+      .then(res => {
+        expect(res.body).toEqual([
+          {
+            _id: expect.anything(),
+            bird: 'sparrow',
+            season: 'fall',
+            spotted: 1,
+            __v: 0
+          }
+        ]);
+      });
+  });
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./lib/app.js');
 
-mongoose.connect('mongodb://localhost:27017/shareables', {
+mongoose.connect('mongodb://localhost:27017/birds', {
   useNewURLParser: true,
   useUnifiedTopology: true
 });
